538568f9e1e1235b4da07a0e079c5295e9768e56
Martin <martin.ennemoser@gmx.at>
2016-01-10 13:46:31 +0100

    Package name changed

--- src/java/restlogic/JSONBuilder.java
+++ src/java/restlogic/JSONBuilder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.number26;
+package restlogic;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
